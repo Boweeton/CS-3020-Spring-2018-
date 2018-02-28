d067462b81f14b0aba8410a12020487dc8c8853a
LAPTOP-UN3TDJJ3\Boweeton <lsoderst@uccs.edu>
2018-02-28 16:44:23 -0700

    Made in class projects

---
+++ In Class Projects/queryTesting/queryTesting/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Linq;
+
+namespace queryTesting
+{
+    public class Class1
+    {
+        static string[] nameArray = { "Joe", "Ken", "Luke", "Errik", "Tom", "Alex", "Jackson", "Tim" };
+
+        var nameQuery = from name in nameArray where name.ElementAt(0) == 'J' orderby name select name;
+    }
+}
---
+++ In Class Projects/queryTesting2/queryTesting2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queryTesting2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Up front declarations
+            Random rng = new Random();
+            List<char> charList = new List<char>();
+
+            // Fill the list with random letters
+            for (int i = 0; i < 1000; i++)
+            {
+                charList.Add((char)rng.Next('A', 'Z' + 1));
+            }
+
+            // LINQ Query to sort decending
+            var qList0 =
+                from qChar in charList
+                orderby qChar descending
+                select qChar;
+
+            // LINQ Query to sort acending
+            var qList1 =
+                from qChar in charList
+                orderby qChar
+                select qChar;
+
+            // LINQ Query to sort acending
+            var qList2 =
+                (from qChar in charList
+                orderby qChar
+                select qChar).Distinct();
+
+            // Print the list
+            foreach (char c in qList2)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
