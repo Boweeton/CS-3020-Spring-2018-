e71a26dd58f99d38cae5946de3abc00e692861ae
Luke <luke.r.strom@gmail.com>
2018-04-18 17:57:37 -0600

    made icc 3

---
+++ In Class Projects/InClassChallenge3/InClassChallenge3/MainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace InClassChallenge3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listSizeTextBox = new System.Windows.Forms.TextBox();
+            this.listSizeLabel = new System.Windows.Forms.Label();
+            this.generateIntsButton = new System.Windows.Forms.Button();
+            this.generateDoublesButton = new System.Windows.Forms.Button();
+            this.listValuesTextBox = new System.Windows.Forms.TextBox();
+            this.listValuesBoxLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchGroupBox = new System.Windows.Forms.GroupBox();
+            this.searchResultsGroupBox = new System.Windows.Forms.GroupBox();
+            this.searchResultsLabel = new System.Windows.Forms.Label();
+            this.searchGroupBox.SuspendLayout();
+            this.searchResultsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listSizeTextBox
+            // 
+            this.listSizeTextBox.Location = new System.Drawing.Point(13, 100);
+            this.listSizeTextBox.Name = "listSizeTextBox";
+            this.listSizeTextBox.Size = new System.Drawing.Size(46, 20);
+            this.listSizeTextBox.TabIndex = 0;
+            // 
+            // listSizeLabel
+            // 
+            this.listSizeLabel.AutoSize = true;
+            this.listSizeLabel.Location = new System.Drawing.Point(13, 81);
+            this.listSizeLabel.Name = "listSizeLabel";
+            this.listSizeLabel.Size = new System.Drawing.Size(46, 13);
+            this.listSizeLabel.TabIndex = 1;
+            this.listSizeLabel.Text = "List Size";
+            // 
+            // generateIntsButton
+            // 
+            this.generateIntsButton.Location = new System.Drawing.Point(13, 13);
+            this.generateIntsButton.Name = "generateIntsButton";
+            this.generateIntsButton.Size = new System.Drawing.Size(118, 23);
+            this.generateIntsButton.TabIndex = 2;
+            this.generateIntsButton.Text = "Fill List With Ints";
+            this.generateIntsButton.UseVisualStyleBackColor = true;
+            this.generateIntsButton.Click += new System.EventHandler(this.OnGenerateIntsButton_Click);
+            // 
+            // generateDoublesButton
+            // 
+            this.generateDoublesButton.Location = new System.Drawing.Point(13, 51);
+            this.generateDoublesButton.Name = "generateDoublesButton";
+            this.generateDoublesButton.Size = new System.Drawing.Size(118, 23);
+            this.generateDoublesButton.TabIndex = 3;
+            this.generateDoublesButton.Text = "Fill List With Doubles";
+            this.generateDoublesButton.UseVisualStyleBackColor = true;
+            this.generateDoublesButton.Click += new System.EventHandler(this.OnGenerateDoublesButton_Click);
+            // 
+            // listValuesTextBox
+            // 
+            this.listValuesTextBox.Location = new System.Drawing.Point(163, 35);
+            this.listValuesTextBox.Multiline = true;
+            this.listValuesTextBox.Name = "listValuesTextBox";
+            this.listValuesTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.listValuesTextBox.Size = new System.Drawing.Size(118, 199);
+            this.listValuesTextBox.TabIndex = 4;
+            // 
+            // listValuesBoxLabel
+            // 
+            this.listValuesBoxLabel.AutoSize = true;
+            this.listValuesBoxLabel.Location = new System.Drawing.Point(195, 19);
+            this.listValuesBoxLabel.Name = "listValuesBoxLabel";
+            this.listValuesBoxLabel.Size = new System.Drawing.Size(58, 13);
+            this.listValuesBoxLabel.TabIndex = 5;
+            this.listValuesBoxLabel.Text = "List Values";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(6, 28);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(91, 20);
+            this.searchTextBox.TabIndex = 6;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(6, 55);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(91, 23);
+            this.searchButton.TabIndex = 8;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.OnSearchButton_Click);
+            // 
+            // searchGroupBox
+            // 
+            this.searchGroupBox.Controls.Add(this.searchButton);
+            this.searchGroupBox.Controls.Add(this.searchTextBox);
+            this.searchGroupBox.Location = new System.Drawing.Point(13, 143);
+            this.searchGroupBox.Name = "searchGroupBox";
+            this.searchGroupBox.Size = new System.Drawing.Size(111, 91);
+            this.searchGroupBox.TabIndex = 9;
+            this.searchGroupBox.TabStop = false;
+            this.searchGroupBox.Text = "Search";
+            // 
+            // searchResultsGroupBox
+            // 
+            this.searchResultsGroupBox.Controls.Add(this.searchResultsLabel);
+            this.searchResultsGroupBox.Location = new System.Drawing.Point(19, 261);
+            this.searchResultsGroupBox.Name = "searchResultsGroupBox";
+            this.searchResultsGroupBox.Size = new System.Drawing.Size(262, 65);
+            this.searchResultsGroupBox.TabIndex = 10;
+            this.searchResultsGroupBox.TabStop = false;
+            this.searchResultsGroupBox.Text = "Search Results";
+            // 
+            // searchResultsLabel
+            // 
+            this.searchResultsLabel.AutoSize = true;
+            this.searchResultsLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchResultsLabel.Location = new System.Drawing.Point(6, 16);
+            this.searchResultsLabel.Name = "searchResultsLabel";
+            this.searchResultsLabel.Size = new System.Drawing.Size(136, 26);
+            this.searchResultsLabel.TabIndex = 0;
+            this.searchResultsLabel.Text = "First Index: _";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 349);
+            this.Controls.Add(this.searchResultsGroupBox);
+            this.Controls.Add(this.searchGroupBox);
+            this.Controls.Add(this.listValuesBoxLabel);
+            this.Controls.Add(this.listValuesTextBox);
+            this.Controls.Add(this.generateDoublesButton);
+            this.Controls.Add(this.generateIntsButton);
+            this.Controls.Add(this.listSizeLabel);
+            this.Controls.Add(this.listSizeTextBox);
+            this.Name = "MainForm";
+            this.Text = "Dumb Search";
+            this.Load += new System.EventHandler(this.OnMainForm_Load);
+            this.searchGroupBox.ResumeLayout(false);
+            this.searchGroupBox.PerformLayout();
+            this.searchResultsGroupBox.ResumeLayout(false);
+            this.searchResultsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox listSizeTextBox;
+        private System.Windows.Forms.Label listSizeLabel;
+        private System.Windows.Forms.Button generateIntsButton;
+        private System.Windows.Forms.Button generateDoublesButton;
+        private System.Windows.Forms.TextBox listValuesTextBox;
+        private System.Windows.Forms.Label listValuesBoxLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.GroupBox searchGroupBox;
+        private System.Windows.Forms.GroupBox searchResultsGroupBox;
+        private System.Windows.Forms.Label searchResultsLabel;
+    }
+}
+
---
+++ In Class Projects/InClassChallenge3/InClassChallenge3/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InClassChallenge3
+{
+    public partial class MainForm : Form
+    {
+        // Fields
+        List<int> listOfIntegers = new List<int>();
+        List<double> listOfDoubles = new List<double>();
+        readonly Random rng = new Random();
+        bool isOnInts;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void OnMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int Search<T>(T key, IReadOnlyList<T> list) where T : IComparable
+        {
+            // Search through the list for the key
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(key) == 0)
+                {
+                    return i;
+                }
+            }
+
+            // Return -1 if the key never matches
+            return -1;
+        }
+
+        void UpdateListBox<T>(List<T> list)
+        {
+            // Local declarations
+            StringBuilder sb = new StringBuilder();
+
+            // Build the string
+            sb.AppendLine(list[0].ToString());
+            for (int i = 1; i < list.Count; i++)
+            {
+                sb.AppendLine(list[i].ToString());
+            }
+
+            // Update the box value
+            listValuesTextBox.Text = sb.ToString();
+        }
+
+        void OnGenerateIntsButton_Click(object sender, EventArgs e)
+        {
+            // Flush the list and find the list size
+            listOfIntegers = new List<int>();
+            int.TryParse(listSizeTextBox.Text, out int listSize);
+
+            // Fill the list
+            for (int i = 0; i < listSize; i++)
+            {
+                listOfIntegers.Add(rng.Next(0,1001));
+            }
+
+            // Update the list box and flip the bools
+            UpdateListBox(listOfIntegers);
+            isOnInts = true;
+        }
+
+        void OnGenerateDoublesButton_Click(object sender, EventArgs e)
+        {
+            // Flush the list and find the list size
+            listOfDoubles = new List<double>();
+            int.TryParse(listSizeTextBox.Text, out int listSize);
+
+            // Fill the list
+            for (int i = 0; i < listSize; i++)
+            {
+                listOfDoubles.Add(rng.Next(0, 101));
+            }
+
+            // Update the list box and flip the bools
+            UpdateListBox(listOfDoubles);
+            isOnInts = false;
+        }
+
+        void OnSearchButton_Click(object sender, EventArgs e)
+        {
+            // Find the key
+            double.TryParse(searchTextBox.Text, out double key);
+
+            // Find the index based on the current bool switch
+            int index = isOnInts ? Search((int)key, listOfIntegers) : Search(key, listOfDoubles);
+
+            // Update the results box
+            searchResultsLabel.Text = index >=0 ? $"FirstIndex: {index}" : $"FirstIndex: (D.N.E.)";
+        }
+    }
+}
